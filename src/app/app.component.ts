c593e9657ff0ef57fbf090af000e4b98363833d1
snehaneosoft <snehaupadhyayneosoft@gmail.com>
2022-05-11 13:27:49 +0530

    ngswitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment20_4Demo';
+  m1:string='50';
+  p1:string='50';
+  c1:string='50';
+  result:number=50;
+  
+  calculatePer(m1:string,p1:string,c1:string){
+
+     this.result= ((parseInt(m1)+parseInt(p1)+parseInt(c1))/300)*100;
+  
+
+  }
+
 }
